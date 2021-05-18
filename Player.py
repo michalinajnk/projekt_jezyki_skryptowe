65ec18f2da7c4f7e92aa71aebf5fbd2020403cc7
LAPTOP-R8BVVF7V\Michalina <254564@student.pwr.edu.pl>
2021-05-18 13:02:39 +0200

    allowing Player to move witihn pressing the button

--- Player.py
+++ Player.py
@@ -4,7 +4,11 @@ class Player(pygame.sprite.Sprite):
 
     def __init__(self, x, y, colour, scale):
         pygame.sprite.Sprite.__init__(self)
-        img = pygame.image.load('Animation/CHARACTER_SPRITES/Black/0.png')
+        img = pygame.image.load(f'Animation/CHARACTER_SPRITES/{colour}/0.png')
         self.img = pygame.transform.scale(img, (int(img.get_width() * scale), int(img.get_height() * scale)))
         self.rect = self.img.get_rect()
         self.rect.center = (x, y)
+
+    def draw(self, screen):
+        screen.blit(self.img, self.rect)
+        pass
